5ef9a17de8cd24a7a0c4b7bbb05013bee21b2cf2
jhoanjimz <jhoan.developer@gmail.com>
2023-08-24 11:19:18 -0500

    rama main

--- lib/pages/pagina1_page.dart
+++ lib/pages/pagina1_page.dart
@@ -1,8 +1,4 @@
-import 'package:estados/bloc/user/user_bloc.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-import '../models/usuario.dart';
 
 
 class Pagina1Page extends StatelessWidget {
@@ -14,22 +10,8 @@ class Pagina1Page extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Pagina 1'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.delete_outline),
-            onPressed: () {
-              BlocProvider.of<UserBloc>(context, listen: false).add(DeleteUsuario());
-            }, 
-          )
-        ],
       ),
-      body: BlocBuilder<UserBloc, UserState>(
-        builder: (_, state) {
-          return state.existUser
-          ? InformacionUsuario(state.user!)
-          : const Center(child: Text('No hay usuario seleccionado'));
-        },
-      ),
+      body: const InformacionUsuario(),
       floatingActionButton: FloatingActionButton(
         onPressed: () => Navigator.pushNamed(context, 'pagina2'),
         child: const Icon(Icons.accessibility_new),
@@ -39,9 +21,7 @@ class Pagina1Page extends StatelessWidget {
 }
 
 class InformacionUsuario extends StatelessWidget {
-  final Usuario user;
-
-  const InformacionUsuario(this.user, {super.key});
+  const InformacionUsuario({ super.key });
 
   @override
   Widget build(BuildContext context) {
@@ -49,23 +29,20 @@ class InformacionUsuario extends StatelessWidget {
       height: double.infinity,
       width: double.infinity,
       padding: const EdgeInsets.all(20.0),
-      child: Column(
+      child: const Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          const Text('General', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-          const Divider(),
-          ListTile(title: Text('Nombre: ${user.nombre}')),
-          ListTile(title: Text('Edad: ${user.edad}')),
+          Text('General', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+          Divider(),
+          ListTile(title: Text('Nombre: ')),
+          ListTile(title: Text('Edad: ')),
 
 
-          const Text('Profesiones', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-          const Divider(),
-
-          ...user.profesiones.map(
-            (profesion) => ListTile(title: Text(profesion))
-          ).toList()
-
-          
+          Text('Profesiones', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+          Divider(),
+          ListTile(title: Text('Profesion 1')),
+          ListTile(title: Text('Profesion 2')),
+          ListTile(title: Text('Profesion 3')),
         ],
       ),
     );
--- lib/pages/pagina2_page.dart
+++ lib/pages/pagina2_page.dart
@@ -1,7 +1,4 @@
-import 'package:estados/bloc/user/user_bloc.dart';
-import 'package:estados/models/usuario.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 
 class Pagina2Page extends StatelessWidget {
@@ -10,9 +7,6 @@ class Pagina2Page extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-    final userBloc = BlocProvider.of<UserBloc>(context);
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Pagina 2'),
@@ -22,24 +16,17 @@ class Pagina2Page extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             MaterialButton(
-              onPressed: () {
-                final newUser = Usuario(
-                  edad: 26, 
-                  nombre: 'Jhoan Jimz', 
-                  profesiones: ['FullStack Developer', 'ProPlayer LoL']
-                );
-                userBloc.add(ActivateUser(newUser));
-              },
+              onPressed: () {},
               color: Colors.blue,
               child: const Text('Establecer Usuario', style: TextStyle(color: Colors.white)),
             ),
             MaterialButton(
-              onPressed: () => userBloc.add(ChangeUserAge(32)),
+              onPressed: () {},
               color: Colors.blue,
               child: const Text('Cambiar Edad', style: TextStyle(color: Colors.white)),
             ),
             MaterialButton(
-              onPressed: () => userBloc.add(AddProfesion('Profesion ${userBloc.state.user!.profesiones.length + 1}')),
+              onPressed: () {},
               color: Colors.blue,
               child: const Text('Añadir Profesion', style: TextStyle(color: Colors.white)),
             )
